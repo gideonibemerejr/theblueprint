7f124d48d4b783d52c5604bbb1da5925e8c263f2
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-07 09:11:50 -0600

    feat: Adds sorting and stuff to admin panel

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -10,10 +10,7 @@ export default function Cards({ data }) {
   const memoizedData = useMemo(() => data, [data]);
 
   return (
-    <ul
-      role='list'
-      className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'
-    >
+    <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
       {memoizedData.map((event) => {
         const query = event?.venue.split(' ').join('+');
 
--- src/containers/AdminDashboard/EventsPage/index.js
+++ src/containers/AdminDashboard/EventsPage/index.js
@@ -4,15 +4,29 @@ import useSWR from 'swr';
 import queryString from 'query-string';
 import { pickBy } from 'lodash';
 import { Switch } from '@headlessui/react';
+import moment from 'moment';
 
-import { AdminEmptyState, Table, Cards, Pagination } from '../../../components';
+import {
+  Table,
+  AdminEmptyState,
+  Cards,
+  Pagination,
+  SortDropdown,
+} from '../../../components';
 import { getEvents } from '../../../services/events';
-import { TABLE_COLUMNS, VIEW_TYPES } from '../../../_constants';
+import {
+  FESTIVAL_DAYS,
+  TABLE_COLUMNS,
+  VIEW_TYPES,
+  SORT_OPTIONS,
+} from '../../../_constants';
 
 const EventsPage = (props) => {
   const [view, setView] = useState(VIEW_TYPES.CARDS);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(25);
+  const [currentDate, setCurrentDate] = useState(FESTIVAL_DAYS[0].value);
+  const [currentSort, setCurrentSort] = useState(SORT_OPTIONS[0].value);
   const nextPage = () => setPage((prevState) => prevState + 1);
   const prevPage = () => setPage((prevState) => prevState - 1);
   const params = queryString.stringify(
@@ -24,7 +38,11 @@ const EventsPage = (props) => {
     }
   );
   const { data, error } = useSWR(
-    `/blue-sheet-events?${params}&pagination[page]=${page}&pagination[pageSize]=${pageSize}`,
+    `/blue-sheet-events?${params}&pagination[page]=${page}&pagination[pageSize]=${pageSize}${
+      currentDate !== '*'
+        ? `&filters[startDate][$eq]=${moment(currentDate).format()}`
+        : ''
+    }${currentSort !== 'none' ? `&sort=${currentSort}` : ''}`,
     getEvents
   );
   const { setCurrentModal, setOpen } = useOutletContext();
@@ -69,11 +87,31 @@ const EventsPage = (props) => {
 
   return (
     <>
-      <div className='flex flex-col mt-4'>
-        <label className='text-gray-300'>{`${
-          view === VIEW_TYPES.CARDS ? 'Card' : 'Table'
-        } View`}</label>
-        <ViewSwitch enabled={view === VIEW_TYPES.CARDS} setEnabled={setView} />
+      <div className='flex sm:items-center justify-between'>
+        <div className='flex flex-col sm:flex-row mt-4'>
+          <div className='mb-4 sm:mr-4'>
+            <SortDropdown
+              label='Festival Day'
+              options={FESTIVAL_DAYS}
+              onChange={setCurrentDate}
+            />
+          </div>
+
+          <SortDropdown
+            label='Sort By'
+            options={SORT_OPTIONS}
+            onChange={setCurrentSort}
+          />
+        </div>
+        <div className='flex flex-col mt-4'>
+          <label className='block text-sm font-medium text-gray-300'>{`${
+            view === VIEW_TYPES.CARDS ? 'Card' : 'Table'
+          } View`}</label>
+          <ViewSwitch
+            enabled={view === VIEW_TYPES.CARDS}
+            setEnabled={setView}
+          />
+        </div>
       </div>
       <div className='px-4 py-8 sm:px-0'>
         {data?.events?.length > 0 ? (
