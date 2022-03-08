d4aba4f9dd80edd24b763cb21b7bd7b72a61e973
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-08 00:33:54 -0600

    feat: Adds sync button for google sheets

--- src/containers/AdminDashboard/index.js
+++ src/containers/AdminDashboard/index.js
@@ -1,45 +1,48 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
-import { AdminHeader, CreateModal } from '../../components';
+import { AdminHeader, CreateModal } from "../../components";
+import httpClient from "../../utils/httpClient";
 
 export default function AdminDashboard() {
-  const [open, setOpen] = useState(false);
-  const [currentModal, setCurrentModal] = useState(null);
-
-  return (
-    <div className='py-10'>
-      <header>
-        <div className=' mx-auto px-4 sm:px-6 lg:px-8'>
-          <AdminHeader>
-            <div className='mt-3 flex md:mt-0 md:absolute md:top-3 md:right-0'>
-              <button
-                onClick={() => {
-                  setCurrentModal('CREATE_EVENT');
-                  setOpen(true);
-                }}
-                type='button'
-                className='inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue'
-              >
-                Create Event
-              </button>
-            </div>
-          </AdminHeader>
-        </div>
-      </header>
-      <main>
-        <div className=' mx-auto sm:px-6 lg:px-8'>
-          {/* Replace with your content */}
-          <Outlet
-            context={{
-              setCurrentModal,
-              setOpen,
-            }}
-          />
-          {/* /End replace */}
-          <CreateModal open={open} setOpen={setOpen} formType={currentModal} />
-        </div>
-      </main>
-    </div>
-  );
+	const [open, setOpen] = useState(false);
+	const [currentModal, setCurrentModal] = useState(null);
+	const handleSync = async () => {
+		const response = httpClient.get("/sync");
+		if (response.status === 200) {
+			console.log(response);
+		}
+	};
+	return (
+		<div className="py-10">
+			<header>
+				<div className=" mx-auto px-4 sm:px-6 lg:px-8">
+					<AdminHeader>
+						<div className="mt-3 flex md:mt-0 md:absolute md:top-3 md:right-0">
+							<button
+								onClick={handleSync}
+								type="button"
+								className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue"
+							>
+								Sync with Google Sheet
+							</button>
+						</div>
+					</AdminHeader>
+				</div>
+			</header>
+			<main>
+				<div className=" mx-auto sm:px-6 lg:px-8">
+					{/* Replace with your content */}
+					<Outlet
+						context={{
+							setCurrentModal,
+							setOpen,
+						}}
+					/>
+					{/* /End replace */}
+					<CreateModal open={open} setOpen={setOpen} formType={currentModal} />
+				</div>
+			</main>
+		</div>
+	);
 }
