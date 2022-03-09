18eb2ee261071c5b820c8edd338517c2dcf497a0
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-09 16:49:02 -0600

    feat: Adds footer

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -60,7 +60,7 @@ const Login = () => {
 
 	return (
 		<>
-			<div className="min-h-screen flex flex-col justify-start py-12 px-6 lg:px-8 bg-primary">
+			<div className="flex flex-col justify-start py-12 px-6 lg:px-8 bg-primary">
 				<div className="sm:mx-auto sm:w-full sm:max-w-md">
 					<h2 className="mt-6 text-center text-3xl font-extrabold text-white">
 						Sign in to your account
