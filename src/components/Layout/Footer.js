57007e96ba5460d8eee09992645f7ecda9d5ccb1
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-09 17:44:06 -0600

    Fix: updates the footer text

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -71,6 +71,7 @@ const navigation = [
 	// },
 ];
 export default function Footer() {
+	const date = new Date().getFullYear();
 	return (
 		<footer className="bg-primary">
 			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
@@ -88,7 +89,7 @@ export default function Footer() {
 				</div>
 				<div className="mt-8 md:mt-0 md:order-1">
 					<p className="text-center text-base text-gray-400">
-						&copy; 2020 Workflow, Inc. All rights reserved.
+						&copy; {date} Purveyors of Now, LLC. All rights reserved.
 					</p>
 				</div>
 			</div>
