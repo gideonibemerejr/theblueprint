cc7763b1ab0a5cdbbaffe4cc9efc9aaad3e6f979
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-09 15:09:37 -0600

    feat: adds donation button

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,19 @@
-export { default as Layout } from './Layout';
-export { default as AdminHeader } from './AdminHeader';
-export { default as CreateModal } from './CreateModal';
-export { default as CreateEventForm } from './CreateEventForm';
-export { default as UserEmptyState } from './UserEmptyState';
-export { default as AdminEmptyState } from './AdminEmptyState';
-export { default as Select } from './Select';
-export { default as DatePicker } from './DatePicker';
-export { default as TimePicker } from './TimePicker';
-export { default as Switch } from './Switch';
-export { default as Table } from './Table';
-export { default as Emoji } from './Emoji';
-export { default as Cards } from './Cards';
-export { default as UserHeader } from './UserHeader';
-export { default as Pagination } from './Pagination';
-export { default as SortDropdown } from './SortDropdown';
+export { default as Layout } from "./Layout";
+export { default as AdminHeader } from "./AdminHeader";
+export { default as CreateModal } from "./CreateModal";
+export { default as CreateEventForm } from "./CreateEventForm";
+export { default as UserEmptyState } from "./UserEmptyState";
+export { default as AdminEmptyState } from "./AdminEmptyState";
+export { default as Select } from "./Select";
+export { default as DatePicker } from "./DatePicker";
+export { default as TimePicker } from "./TimePicker";
+export { default as Switch } from "./Switch";
+export { default as Table } from "./Table";
+export { default as Emoji } from "./Emoji";
+export { default as Cards } from "./Cards";
+export { default as UserHeader } from "./UserHeader";
+export { default as Pagination } from "./Pagination";
+export { default as SortDropdown } from "./SortDropdown";
+
+// General
+export { default as Button } from "./Button";
