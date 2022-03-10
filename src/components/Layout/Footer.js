07c2327b82316a9fcb72aed55c5a9353d7f305ff
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-03-09 18:14:56 -0600

    refactor: makes footer full width

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -74,7 +74,7 @@ export default function Footer() {
 	const date = new Date().getFullYear();
 	return (
 		<footer className="bg-primary">
-			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+			<div className="mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
 				<div className="flex justify-center space-x-6 md:order-2">
 					{navigation.map((item) => (
 						<a
